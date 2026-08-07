19936efc52f12bc2c913f591eaf8f64879b0bb0b
Ethan Henderson <ethan.henderson.1998@gmail.com>
2026-08-08 00:17:39 +0100

    Create backbone of main page

--- src/App.vue
+++ src/App.vue
@@ -1,160 +1,71 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
 
-const greetMsg = ref("");
-const name = ref("");
+import Card from "openvue/card";
+import FileUpload from "openvue/fileupload";
+import Message from "openvue/message";
+import { FilePlus, FolderOpen } from "@lucide/vue";
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", { name: name.value });
-}
+const fileupload = ref();
+const messages = ref<string[]>([]);
+
+const onChoose = () => {
+    fileupload.value.choose();
+};
+
+const onUpload = () => {
+    messages.value = ["Uploaded!"];
+};
+
+const onSelect = () => {
+    const uploadMessages = fileupload.value.messages as string[];
+    if (!uploadMessages) return;
+
+    messages.value = uploadMessages;
+    fileupload.value.messages = [];
+};
 </script>
 
 <template>
-  <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
+    <div class="flex min-h-screen flex-col items-center justify-center gap-4">
+        <FileUpload
+            ref="fileupload"
+            class="hidden"
+            mode="basic"
+            :auto="true"
+            accept=".parquet"
+            invalidFileTypeMessage="Chevron only supports Parquet files."
+            custom-upload
+            @select="onSelect"
+            @uploader="onUpload"
+        />
 
-    <div class="row">
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
+        <span class="text-3xl font-bold">Chevron</span>
+        <span class="text-lg">What do you want to do?</span>
+
+        <Message :key="msg" v-for="msg of messages" severity="error">
+            {{ msg }}
+        </Message>
+
+        <div class="flex flex-row gap-4">
+            <Card class="w-40 opacity-50">
+                <template #content>
+                    <div class="flex flex-col items-center gap-2">
+                        <FilePlus :size="48" />
+                        <span class="text-lg">New</span>
+                    </div>
+                </template>
+            </Card>
+            <Card class="w-40" @click="onChoose">
+                <template #content>
+                    <div
+                        class="flex flex-col items-center gap-2 cursor-pointer"
+                    >
+                        <FolderOpen :size="48" />
+                        <span class="text-lg">Open</span>
+                    </div>
+                </template>
+            </Card>
+        </div>
     </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <form class="row" @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
-    </form>
-    <p>{{ greetMsg }}</p>
-  </main>
 </template>
-
-<style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
-
-</style>
-<style>
-:root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
-
-  color: #0f0f0f;
-  background-color: #f6f6f6;
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
-
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-
-a:hover {
-  color: #535bf2;
-}
-
-h1 {
-  text-align: center;
-}
-
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-button {
-  cursor: pointer;
-}
-
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
-
-input,
-button {
-  outline: none;
-}
-
-#greet-input {
-  margin-right: 5px;
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
-
-  a:hover {
-    color: #24c8db;
-  }
-
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
-}
-
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./assets/css/global.css";
+import OpenVue from "openvue/config";
+import Aura from "@openvue/themes/aura";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(OpenVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      cssLayer: {
+        name: "openvue",
+        order: "theme, base, openvue, components, utilities",
+      },
+    },
+  },
+});
+app.mount("#app");
