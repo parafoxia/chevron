b22263ca4a07e293306de817712561fcbf5f649c
Ethan Henderson <ethan.henderson.1998@gmail.com>
2026-08-08 00:32:01 +0100

    Swap out FileDialog for Tauri's own solution

--- src/App.vue
+++ src/App.vue
@@ -2,50 +2,29 @@
 import { ref } from "vue";
 
 import Card from "openvue/card";
-import FileUpload from "openvue/fileupload";
-import Message from "openvue/message";
 import { FilePlus, FolderOpen } from "@lucide/vue";
+import { open } from "@tauri-apps/plugin-dialog";
 
-const fileupload = ref();
-const messages = ref<string[]>([]);
+const path = ref<string>();
 
-const onChoose = () => {
-    fileupload.value.choose();
-};
+const onOpen = async () => {
+    const result = await open({
+        title: "Select a file",
+        filters: [{ name: "Parquet", extensions: ["parquet"] }],
+    });
 
-const onUpload = () => {
-    messages.value = ["Uploaded!"];
-};
-
-const onSelect = () => {
-    const uploadMessages = fileupload.value.messages as string[];
-    if (!uploadMessages) return;
-
-    messages.value = uploadMessages;
-    fileupload.value.messages = [];
+    if (result) {
+        path.value = result;
+    }
 };
 </script>
 
 <template>
     <div class="flex min-h-screen flex-col items-center justify-center gap-4">
-        <FileUpload
-            ref="fileupload"
-            class="hidden"
-            mode="basic"
-            :auto="true"
-            accept=".parquet"
-            invalidFileTypeMessage="Chevron only supports Parquet files."
-            custom-upload
-            @select="onSelect"
-            @uploader="onUpload"
-        />
-
         <span class="text-3xl font-bold">Chevron</span>
         <span class="text-lg">What do you want to do?</span>
 
-        <Message :key="msg" v-for="msg of messages" severity="error">
-            {{ msg }}
-        </Message>
+        {{ path }}
 
         <div class="flex flex-row gap-4">
             <Card class="w-40 opacity-50">
@@ -56,7 +35,7 @@ const onSelect = () => {
                     </div>
                 </template>
             </Card>
-            <Card class="w-40" @click="onChoose">
+            <Card class="w-40" @click="onOpen">
                 <template #content>
                     <div
                         class="flex flex-col items-center gap-2 cursor-pointer"
