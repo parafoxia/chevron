4448dc29976b91268399eb9bce57a1dd1ec9eb36
Ethan Henderson <ethan.henderson.1998@gmail.com>
2026-08-07 21:22:43 +0100

    Work around Error 71 on NVIDIA + Wayland setups

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,20 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
+    #[cfg(target_os = "linux")]
+    disable_nvidia_explicit_sync();
+
     chevron_lib::run()
+}
+
+// Workaround for NVIDIA + Wayland setups getting Error 71s.
+// See https://github.com/tauri-apps/tauri/issues/10702 for the open issue.
+#[cfg(target_os = "linux")]
+fn disable_nvidia_explicit_sync() {
+    if std::env::var_os("WAYLAND_DISPLAY").is_some()
+        && std::env::var_os("__NV_DISABLE_EXPLICIT_SYNC").is_none()
+    {
+        std::env::set_var("__NV_DISABLE_EXPLICIT_SYNC", "1");
+    }
 }
