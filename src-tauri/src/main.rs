b2460535de995a0bfcd6abceec8c1e302dcdc616
Ethan Henderson <ethan.henderson.1998@gmail.com>
2026-08-09 19:04:52 +0100

    Hopefully fix problems with MacOS and AppImage versions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -17,26 +17,24 @@ fn main() {
 #[cfg(target_os = "linux")]
 fn apply_linux_display_workarounds() {
     let on_wayland = std::env::var_os("WAYLAND_DISPLAY").is_some();
+    let in_appimage = std::env::var_os("APPDIR").is_some();
 
     // Force the GDK_BACKEND to use Wayland in AppImages.
     if on_wayland
         && std::env::var_os("CHEVRON_FORCE_X11").is_none()
-        && std::env::var_os("APPDIR").is_some()
+        && in_appimage
         && std::env::var("GDK_BACKEND").as_deref() == Ok("x11")
     {
         std::env::set_var("GDK_BACKEND", "wayland");
     }
 
     // Force dark theme.
-    if std::env::var_os("APPDIR").is_some() && std::env::var_os("APPIMAGE_GTK_THEME").is_none() {
+    if in_appimage && std::env::var_os("APPIMAGE_GTK_THEME").is_none() {
         std::env::set_var("GTK_THEME", "Adwaita:dark");
     }
 
-    let on_x11 = !on_wayland || std::env::var("GDK_BACKEND").as_deref() == Ok("x11");
-
-    // Stop the webview rendering blank on NVIDIA under X11.
-    if on_x11
-        && std::path::Path::new("/dev/nvidiactl").exists()
+    // Stop the webview rendering blank or crashing on startup on NVIDIA.
+    if std::path::Path::new("/dev/nvidiactl").exists()
         && std::env::var_os("WEBKIT_DISABLE_DMABUF_RENDERER").is_none()
     {
         std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
